25e1060cd77150c0a64753ec836f671ff6574ff6
yoosang <yoo7969@naver.com>
2022-11-11 11:23:39 +0900

    Feat: Create Project

---
+++ src/main/java/com/example/InsuranceSystem_Web/InsuranceSystemWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.InsuranceSystem_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceSystemWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceSystemWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/InsuranceSystem_Web/InsuranceSystemWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InsuranceSystem_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceSystemWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
