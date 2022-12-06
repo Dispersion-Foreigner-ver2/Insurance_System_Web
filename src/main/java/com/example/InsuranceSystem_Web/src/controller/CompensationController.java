301f956dec0f39557052bd8f77ab1af7444554ff
sangminee <lsmlee99@gmail.com>
2022-12-06 17:45:35 +0900

    feat : contract merge

--- src/main/java/com/example/InsuranceSystem_Web/src/controller/CompensationController.java
+++ src/main/java/com/example/InsuranceSystem_Web/src/controller/CompensationController.java
@@ -1,4 +1,12 @@
 package com.example.InsuranceSystem_Web.src.controller;
 
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
 public class CompensationController {
 }
